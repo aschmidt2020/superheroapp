a1485477a0a0d02f2a3faac32e1fff3ab4137b6a
Audrey Schmidt <audsch@outlook.com>
2021-12-09 15:24:18 -0600

    made create.html and create view logic to allow user to add a new superhero

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from superheroes.models import Superhero
 
 # Create your views here.
@@ -13,4 +13,21 @@ def detail(request, hero_id):
     single_hero = Superhero.objects.get(pk=hero_id)
     context = {'single_hero': single_hero}
     return render (request, 'superheroes/detail.html', context)
-    
+
+def create(request):
+    if request.method == "POST":
+        #save the form contents as a new db object by instantiating new hero
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary_ability = request.POST.get('primary_ability')
+        secondary_ability = request.POST.get('secondary_ability')
+        catchphrase = request.POST.get('catchphrase')
+        
+        new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary_ability, secondary_ability=secondary_ability, catchphrase=catchphrase)
+        new_hero.save()
+        
+        #return to index.html
+        return HttpResponseRedirect(reverse('superheroes:index'))
+        
+    else:
+        return render(request, 'superheroes/create.html')
