47f2e6440a5c7c9606d9db6619036884aea25ce1
Audrey Schmidt <audsch@outlook.com>
2021-12-10 11:09:02 -0600

    added form actions for update and delete.html

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'superheroes'
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('<int:hero_id>/', views.detail, name='detail'),
+    path('<int:hero_id>/details', views.detail, name='detail'),
     path('new/', views.create, name='create'),
     path('<int:hero_id>/delete', views.delete, name='delete'),
     path('<int:hero_id>/update', views.update, name='update'),
